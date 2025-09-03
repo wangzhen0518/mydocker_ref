xianlubird/mydocker

fix missing of resource command & cgroup manager (#39)
* fix missing of resource command & cgroup manager

* Update main_command.go

* Update run.go

main_command.go
@@ -5,6 +5,7 @@ import (
 	log "github.com/Sirupsen/logrus"
 	"github.com/urfave/cli"
 	"github.com/xianlubird/mydocker/container"
+	"github.com/xianlubird/mydocker/cgroups/subsystems"
 )
 
 var runCommand = cli.Command{
@@ -16,6 +17,18 @@ var runCommand = cli.Command{
 			Name:  "ti",
 			Usage: "enable tty",
 		},
+		cli.StringFlag{
+			Name: "m",
+			Usage: "memory limit",
+		},
+		cli.StringFlag{
+			Name: "cpushare",
+			Usage: "cpushare limit",
+		},
+		cli.StringFlag{
+			Name: "cpuset",
+			Usage: "cpuset limit",
+		},
 	},
 	Action: func(context *cli.Context) error {
 		if len(context.Args()) < 1 {
@@ -26,7 +39,12 @@ var runCommand = cli.Command{
 			cmdArray = append(cmdArray, arg)
 		}
 		tty := context.Bool("ti")
-		Run(tty, cmdArray)
+		resConf := &subsystems.ResourceConfig{
+			MemoryLimit: context.String("m"),
+			CpuSet: context.String("cpuset"),
+			CpuShare:context.String("cpushare"),
+		}
+		Run(tty, cmdArray, resConf)
 		return nil
 	},
 }

run.go
@@ -5,10 +5,12 @@ import (
 	log "github.com/Sirupsen/logrus"
 	"os"
 	"strings"
+	"github.com/xianlubird/mydocker/cgroups/subsystems"
+	"github.com/xianlubird/mydocker/cgroups"
 )
 
 
-func Run(tty bool, comArray []string) {
+func Run(tty bool, comArray []string, res *subsystems.ResourceConfig) {
 	parent, writePipe := container.NewParentProcess(tty)
 	if parent == nil {
 		log.Errorf("New parent process error")
@@ -17,6 +19,10 @@ func Run(tty bool, comArray []string) {
 	if err := parent.Start(); err != nil {
 		log.Error(err)
 	}
+	cgroupManager := cgroups.NewCgroupManager("donkey-cgroup")
+	defer cgroupManager.Destroy()
+	cgroupManager.Set(res)
+	cgroupManager.Apply(parent.Process.Pid)
 	sendInitCommand(comArray, writePipe)
 	parent.Wait()
 	os.Exit(0)

wangzhen0518/mydocker_ref

main_command.go
@@ -5,6 +5,7 @@ import (
 
 	log "github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
+	"github.com/wangzhen0518/mydocker_ref/cgroups/subsystems"
 	"github.com/wangzhen0518/mydocker_ref/container"
 )
 
@@ -17,6 +18,18 @@ var runCommand = cli.Command{
 			Name:  "ti",
 			Usage: "enable tty",
 		},
+		cli.StringFlag{
+			Name:  "m",
+			Usage: "memory limit",
+		},
+		cli.StringFlag{
+			Name:  "cpushare",
+			Usage: "cpushare limit",
+		},
+		cli.StringFlag{
+			Name:  "cpuset",
+			Usage: "cpuset limit",
+		},
 	},
 	Action: func(context *cli.Context) error {
 		if len(context.Args()) < 1 {
@@ -27,7 +40,12 @@ var runCommand = cli.Command{
 			cmdArray = append(cmdArray, arg)
 		}
 		tty := context.Bool("ti")
-		Run(tty, cmdArray)
+		resConf := &subsystems.ResourceConfig{
+			MemoryLimit: context.String("m"),
+			CpuSet:      context.String("cpuset"),
+			CpuShare:    context.String("cpushare"),
+		}
+		Run(tty, cmdArray, resConf)
 		return nil
 	},
 }

run.go
@@ -5,10 +5,12 @@ import (
 	"strings"
 
 	log "github.com/sirupsen/logrus"
+	"github.com/wangzhen0518/mydocker_ref/cgroups"
+	"github.com/wangzhen0518/mydocker_ref/cgroups/subsystems"
 	"github.com/wangzhen0518/mydocker_ref/container"
 )
 
-func Run(tty bool, comArray []string) {
+func Run(tty bool, comArray []string, res *subsystems.ResourceConfig) {
 	parent, writePipe := container.NewParentProcess(tty)
 	if parent == nil {
 		log.Errorf("New parent process error")
@@ -17,6 +19,10 @@ func Run(tty bool, comArray []string) {
 	if err := parent.Start(); err != nil {
 		log.Error(err)
 	}
+	cgroupManager := cgroups.NewCgroupManager("donkey-cgroup")
+	defer cgroupManager.Destroy()
+	cgroupManager.Set(res)
+	cgroupManager.Apply(parent.Process.Pid)
 	sendInitCommand(comArray, writePipe)
 	parent.Wait()
 	os.Exit(0)